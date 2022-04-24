a9d8cff503aa40f9953ad2884ed5709aead9f5e0
管仁阳 <695401454@qq.com>
2022-04-24 18:00:06 +0800

    finish basic model

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -9,39 +9,27 @@ from torch.utils.data import Dataset
 import numpy as np
 import torch
 
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
-
 class MotionSenseDataset(Dataset):
-    def __init__(self, data_dir, train: bool, time_slice):
+    def __init__(self, data_dir,num_features, train: bool, time_slice):
         if train:
-            self.data = torch.from_numpy(np.load(data_dir+"/preprocessed/train.npy"))[:, :-1] # the laset column is gender
+            self.data = torch.from_numpy(np.load(data_dir+"/preprocessed/train.npy"))[:, :-1].float() # the laset column is gender
         else:
-            self.data = torch.from_numpy(np.load(data_dir+"/preprocessed/test.npy"))[:, :-1]
+            self.data = torch.from_numpy(np.load(data_dir+"/preprocessed/test.npy"))[:, :-1].float()
         self.time_slice = time_slice
+        self.num_features = num_features
     
     def __len__(self):
         return (self.data.shape[0] - self.time_slice + 1) # the whole temporal input data as an input
     def __getitem__(self, idx):
-        data = (self.data[idx:idx+self.time_slice, :-6], self.data[idx:idx+self.time_slice, -6:])
+        data = (self.data[idx:idx+self.time_slice, :self.num_features].unsqueeze(0), self.data[idx, self.num_features].long())
         return data
         
-class MotionSenseEncoderDataLoader(BaseDataLoader):
+class MotionSenseDataLoader(BaseDataLoader):
     """
     Motion Sense data loading using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, time_slice,  shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, data_dir, batch_size, time_slice, num_features,  shuffle=True, validation_split=0.0, num_workers=1, training=True):
         self.data_dir = data_dir
-        self.dataset = MotionSenseDataset(data_dir, training, time_slice)
+        self.dataset = MotionSenseDataset(data_dir,num_features, training, time_slice)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
         
--- model/model.py
+++ model/model.py
@@ -1,3 +1,6 @@
+from os import lstat
+from time import time
+from torch import lstm
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
@@ -19,4 +22,46 @@ class MnistModel(BaseModel):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
+
         return F.log_softmax(x, dim=1)
+
+class MotionSenseModel(BaseModel):
+    def __init__(self, time_slice:int, dim_latent:int, dim_lstm_hidden:int):
+        super().__init__()
+        self.num_classes = 6
+        self.time_slice = time_slice
+        self.dim_latent =dim_latent
+        self.dim_lstm_hidden = dim_lstm_hidden
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding='same')
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding='same')
+        self.conv2_drop = nn.Dropout2d()
+        
+        assert time_slice%4==0
+
+        self.dim_per_batch = (time_slice//4) * 3 * 64
+
+        self.fc1 = nn.Linear(self.dim_per_batch, time_slice*dim_latent)
+
+        self.lstm = nn.LSTM(input_size=dim_latent, hidden_size=dim_lstm_hidden, num_layers=1, batch_first=True)
+
+        self.fc2 = nn.Linear(dim_lstm_hidden, 6)
+        
+    def forward(self, x):
+        # Encoder
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2)).view(-1, self.dim_per_batch)
+
+        x = F.relu(self.fc1(x)).view(-1, self.time_slice, self.dim_latent)#.unsqueeze(-1)
+
+        #Decoder
+        _, (hn, _) = self.lstm(x)
+
+        x = hn.squeeze(0)
+
+        x = self.fc2(x)
+        
+        return F.log_softmax(x, dim=1)
+        
+
